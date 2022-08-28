52298ad32fe7c62d6f897153773fd880b30e25bb
Tomasz Kalus <tomaszkalus@interia.pl>
2022-08-29 00:07:58 +0200

    MiniMax algorithm added

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const GameBoard = (function () {
     const _dimension = 3;
     const _board = Array(_dimension ** 2).fill('')
 
-    const place_mark = function (field, symbol) {
+    const placeMark = function (field, symbol) {
         if (!_board[field]) {
             _board[field] = symbol;
             return true;
@@ -14,17 +14,16 @@ const GameBoard = (function () {
         return _board[field]
     }
 
-    const getLegalFields = function () {
-        const indexes = Array.from({ length: _board.length }, (x, i) => i);
-        return indexes.filter(i => { return _board[i] == '' });
+    const getLegalFields = function (board = _board) {
+        const indexes = Array.from({ length: board.length }, (x, i) => i);
+        return indexes.filter(i => { return board[i] == '' });
 
     }
 
-    const checkIfWin = function () {
+    const checkIfWin = function (board = _board) {
         for (let i = 0; i < _dimension; i++) {
             // check rows
-            const row = _board.slice(i * _dimension, (i + 1) * _dimension);
-
+            const row = board.slice(i * _dimension, (i + 1) * _dimension);
             if (row.every(v => v === row[0]) && !row.some(v => v == '')) {
                 return Array.from({ length: _dimension }, (x, index) => index + (i * _dimension));
             };
@@ -33,7 +32,7 @@ const GameBoard = (function () {
             const col = [];
             for (let j = 0; j < _dimension; j++) {
                 const pos = (i + _dimension * j)
-                col.push(_board[pos]);
+                col.push(board[pos]);
             }
             if (col.every(v => v === col[0]) && !col.some(v => v == '')) {
                 return Array.from({ length: _dimension }, (x, index) => (index * _dimension) + i);
@@ -43,8 +42,8 @@ const GameBoard = (function () {
         const ldiag = Array.from({ length: _dimension }, (x, i) => i * (_dimension + 1));
         const rdiag = Array.from({ length: _dimension }, (x, i) => (_dimension - 1) * (i + 1));
 
-        const ldiag_values = ldiag.map((j) => { return _board[j] });
-        const rdiag_values = rdiag.map((j) => { return _board[j] });
+        const ldiag_values = ldiag.map((j) => { return board[j] });
+        const rdiag_values = rdiag.map((j) => { return board[j] });
 
         if (ldiag_values.every(v => v == ldiag_values[0]) && !ldiag_values.some(v => v == '')) { return ldiag }
         if (rdiag_values.every(v => v == rdiag_values[0]) && !rdiag_values.some(v => v == '')) { return rdiag }
@@ -56,13 +55,21 @@ const GameBoard = (function () {
         return false
     }
 
-    const clear_board = function () {
+    const clearBoard = function () {
         _board.forEach((val, index) => { _board[index] = '' })
 
     }
 
+    const getCopy = () => {
+        copy = []
+        for(let i=0; i<9; i++){
+            copy.push(getElement(i))
+        }
+        return copy
+    }
+
     const getDimension = function () { return _dimension }
-    return { place_mark, getElement, checkIfWin, checkIfTie, clear_board, getDimension, getLegalFields }
+    return { placeMark, getElement, checkIfWin, checkIfTie, clearBoard, getDimension, getLegalFields, getCopy }
 
 })()
 
@@ -136,7 +143,7 @@ const Game = (function () {
     }
     const NewGame = function () {
         _player_text.classList.remove('info')
-        _board.clear_board();
+        _board.clearBoard();
         _game_over = false;
         _current_player = _player_list[0];
         updatePlayerText();
@@ -163,7 +170,7 @@ const Game = (function () {
 
     const Move = function (field) {
         _symbol = _current_player.getSymbol()
-        if (_board.place_mark(field, _symbol) && !_game_over) {
+        if (_board.placeMark(field, _symbol) && !_game_over) {
             RenderBoard();
             const _winning_cells = _board.checkIfWin()
             if (_winning_cells) {
@@ -189,9 +196,11 @@ const Game = (function () {
     const UserMove = function (field) {
         if (Move(field)) {
             if (_ai) {
-                const _legal_fields = _board.getLegalFields()
-                const rand = Math.floor(Math.random() * _legal_fields.length);
-                setTimeout(() => Move(_legal_fields[rand]), 100)
+                // const _legal_fields = _board.getLegalFields()
+                // const rand = Math.floor(Math.random() * _legal_fields.length);
+                // setTimeout(() => Move(_legal_fields[rand]), 100)
+
+                Move(MiniMax(_board.getCopy()))
             }
         }
     }
@@ -204,60 +213,156 @@ const Game = (function () {
         updatePlayerText();
     }
 
-    const MiniMax = (function(){
+    const MiniMax = function(board){
 
-        let _board_copy = []
+        let player = 'X', opponent = 'O';
 
-        const _getBoardCopy = () => {
-            _board_copy = []
-            for(let i=0; i<9; i++){
-                _board_copy.push(_board[i])
-            }
+        // This function returns true if there are moves
+        // remaining on the board. It returns false if
+        // there are no moves left to play.
+        function isMovesLeft(board) {
+            if (board.some(v => { v == '' })) { return true }
+            return false;
         }
 
-        const evaluate = function(){
-            const win = checkIfWin();
-            if(win){
-                if(_board.getElement[win[0]] = 'x'){
-                    return 10
+        // This is the evaluation function as discussed
+        // in the previous article ( http://goo.gl/sJgv68 )
+        function evaluate(b, depth) {
+            for (let i = 0; i < 3; i++) {
+                // check rows
+                const row = b.slice(i * 3, (i + 1) * 3);
+                if (row[0] == row[1] && row[1] == row[2] && row[0] != '') {
+                    if (row[0] == player) {
+                        return 10 - depth;
+                    }
+                    else if (row[0] == opponent) {
+                        return depth -10;
+                    }
+                };
+                // check cols
+                const col = [b[i], b[i+3], b[i+6]]
+                if (col[0] == col[1] && col[1] == col[2] && col[0] != '') {
+                    if (col[0] == player) {
+                        return 10 - depth;
+                    }
+                    else if (col[0] == opponent) {
+                        return depth -10;
+                    }
+                };
+            }
+            // check diagonals
+            if (b[0] == b[4] && b[4] == b[8] && b[0] !='') {
+                if (b[0] == player) {
+                    return 10 - depth;
                 }
-                else{return -10}
+                else if (b[0] == opponent) {
+                    return depth -10;
+                }
             }
-            return 0
+            if (b[2] == b[4] && b[4] == b[6] && b[2] !='') {
+                if (b[2] == player) {
+                    return 10 - depth;
+                }
+                else if (b[2] == opponent) {
+                    return depth -10;
+                }
+            }
+
+            return 0;
         }
 
-        const minimax = function(board, depth, index){
-            let score = evaluate(_board);
-            if (score==10){return score}
-            if (score == -10){return score}
-            if (_board.getLegalFields.length = 0){return 0}
-            const isMax = _current_player = _player_list[2]
-            if (isMax){
+        // This is the minimax function. It
+        // considers all the possible ways
+        // the game can go and returns the
+        // value of the board
+        function minimax(board, depth, isMax) {
+            let score = evaluate(board,depth);
+
+            // If Maximizer has won the game
+            // return his/her evaluated score
+            if (score == 10)
+                return score;
+
+            // If Minimizer has won the game
+            // return his/her evaluated score
+            if (score == -10)
+                return score;
+
+            // If there are no more moves and
+            // no winner then it is a tie
+            if (isMovesLeft(board) == false)
+                return 0;
+
+            // If this maximizer's move
+            if (isMax) {
                 let best = -1000;
-                
-                for(let i=0; i<9; i++){
-                    if(!_board[i]){
-                        Move(i)
-
-                        best = Math.max(best, minimax(_board, depth+1, !isMax));
-
+                for (let i = 0; i < 9; i++) {
+                    if (board[i] == '') {
+                        board[i] = player;
+                        best = Math.max(best, minimax(board,
+                            depth + 1, !isMax));
+                        board[i] = '';
                     }
                 }
-
+                return best;
             }
-            else{
-                let best = 1000;
-                for(let i=0; i<9; i++){
-                    if(!_board[i]){
-                        Move(i)
-                        best = Math.max(best, minimax(_board, depth+1, !isMax));
 
+            // If this minimizer's move
+            else {
+                let best = -1000;
+                for (let i = 0; i < 9; i++) {
+                    if (board[i] == '') {
+                        board[i] = player;
+                        best = Math.max(best, minimax(board,
+                            depth + 1, !isMax));
+                        board[i] = '';
                     }
                 }
+                return best;
             }
         }
 
-        return {evaluate}
-    })()
+        // This will return the best possible
+        // move for the player
+        function findBestMove(board) {
+            let bestVal = -1000;
+            let bestMove;
+            bestMove = -1;
+
+            // Traverse all cells, evaluate
+            // minimax function for all empty
+            // cells. And return the cell
+            // with optimal value.
+            for (let i = 0; i < 9; i++) {
+                // Check if cell is empty
+                if (board[i] == '') {
+
+                    // Make the move
+                    board[i] = player;
+
+                    // compute evaluation function
+                    // for this move.
+                    let moveVal = minimax(board, 0, false);
+
+                    // Undo the move
+                    board[i] = '';
+
+                    // If the value of the current move
+                    // is more than the best value, then
+                    // update best
+                    if (moveVal > bestVal) {
+                        bestMove = i;
+                        bestVal = moveVal;
+                    }
+                }
+                
+            }
+
+            return bestMove;
+        }
+        return findBestMove(board);
+
+    }
+
 
 })()
